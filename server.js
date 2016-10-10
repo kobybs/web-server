365b21acc01ace1747ea1185cf1431dde7ff4fb6
Kobybs <whatever>
2016-10-10 17:19:29 +0300

    Initialize repo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+		requireAuth: function(req, res, next){
+			console.log('private route hit');
+			next();
+		},
+		logger: function (req, res, next){		
+			console.log('req: ' + new Date().toString() + req.method + ' ' + req.originalUrl);
+			next();
+		} 
+}
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuth, function (req, res) {
+	res.send('about us');
+} );
+
+app.use(express.static(__dirname + '/public'));
+//console.log(__dirname);
+app.listen(PORT, function (){
+	console.log('exp server started on port ' + PORT);
+} );
